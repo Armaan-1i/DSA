7a17ce3c738d278bbd0f08da96e099d90a7487ed
Arman-1i <mdarman92075@gmail.com>
2026-08-27 10:38:17 +0530

    Time: 9 ms (40.05%) | Memory: 46.8 MB (53.11%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String temp = s + "#" + rev;
+        int[] lps = new int[temp.length()];
+        for (int i = 1; i < temp.length(); i++) {
+            int j = lps[i - 1];
+            while (j > 0 && temp.charAt(i) != temp.charAt(j)) {
+                j = lps[j - 1];
+            }
+            if (temp.charAt(i) == temp.charAt(j)) {
+                j++;
+            }
+            lps[i] = j;
+        }
+        int len = lps[temp.length() - 1];
+        String add = s.substring(len);
+        StringBuilder ans = new StringBuilder(add);
+        ans.reverse();
+        return ans.toString() + s;
+    }
+}
