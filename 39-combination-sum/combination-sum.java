5a6a4b1f9d97f3590836df2779932bee17aee0e9
Arman-1i <mdarman92075@gmail.com>
2026-08-29 09:47:46 +0530

    Time: 2 ms (95.62%) | Memory: 45.5 MB (83.11%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,36 @@
 class Solution {
-    void fun(int[]nums, int target, int start,List<Integer> output, List<List<Integer>> ans){
-        if(target == 0){
+
+    void fun(int[] nums, int target, int i,
+             List<Integer> output,
+             List<List<Integer>> ans) {
+
+        if (target == 0) {
             ans.add(new ArrayList<>(output));
             return;
         }
-        for(int i = start; i< nums.length; i++){
-            if(nums[i] > target){
-                break;
-            }
-            output.add(nums[i]);
-            fun(nums, target - nums[i], i,output,ans);
-            output.remove(output.size() - 1);
+
+        if (i == nums.length || target < 0) {
+            return;
         }
+
+        // Take
+        output.add(nums[i]);
+
+        fun(nums, target - nums[i], i, output, ans);
+
+        output.remove(output.size() - 1);
+
+        // Not Take
+        fun(nums, target, i + 1, output, ans);
     }
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
-        Arrays.sort(nums);
-        fun(nums, target, 0, output,ans);
+
+        fun(candidates, target, 0, output, ans);
+
         return ans;
-        
     }
 }
